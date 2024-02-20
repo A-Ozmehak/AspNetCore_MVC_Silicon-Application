4198046ba7e9d084bcd07660b152cbb53e769450
Anna <nakupanda89@gmail.com>
2024-02-20 15:17:48 +0100

    tools done

---
+++ AspNetCore_MVC/ViewModels/Components/ToolViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetCore_MVC.ViewModels.Components;
+
+public class ToolViewModel
+{
+    public ImageViewModel Image { get; set; } = null!;
+    public string Text { get; set; } = null!;
+}
