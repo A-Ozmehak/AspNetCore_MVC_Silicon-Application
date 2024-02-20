d5a6aa705edbdf2254935fc6d3a16864ecc4ee61
Anna <nakupanda89@gmail.com>
2024-02-20 12:48:36 +0100

    features done

--- AspNetCore_MVC/Controllers/HomeController.cs
+++ AspNetCore_MVC/Controllers/HomeController.cs
@@ -27,8 +27,24 @@ public class HomeController : Controller
                        new() { ImageUrl = "/images/brands/brand3.svg", AltText = "Brand name 3" },
                        new() { ImageUrl = "/images/brands/brand4.svg", AltText = "Brand name 4" }
                    ],
+            },
+            Features = new FeaturesViewModel
+            {
+                Id = "features",
+                Title = "What Do You Get with Our Tool?",
+                Text = "Make sure all your tasks are organized so you can set the priorities and focus on important.",
+                Box =
+                [
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/comments-icon.svg", AltText = "" }, Title = "Comments on Tasks", Description = "Id mollis consectetur congue egestas egestas suspendisse blandit justo." },
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/tasks-analytics-icon.svg", AltText = "" }, Title = "Tasks Analytics", Description = "Non imperdiet facilisis nulla tellus Morbi scelerisque eget adipiscing vulputate." },
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/multiple-assignees-icon.svg", AltText = "" }, Title = "Multiple Assignees", Description = "A elementum, imperdiet enim, pretium etiam facilisi in aenean quam mauris." },
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/notifications-icon.svg", AltText = "" }, Title = "Notifications", Description = "Diam, suspendisse velit cras ac. Lobortis diam volutpat, eget pellentesque viverra." },
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/sections-icon.svg", AltText = "" }, Title = "Sections & Subtasks", Description = "Mi feugiat hac id in. Sit elit placerat lacus nibh lorem ridiculus lectus." },
+                  new() { BoxImage = new() { ImageUrl = "/images/icons/data-security-icon.svg", AltText = "" }, Title = "Data Security", Description = "Aliquam malesuada neque eget elit nulla vestibulum nunc cras." },
+                ]
             }
         };
+
         ViewData["Title"] = viewModel.Title;
         return View(viewModel);
     }
---
+++ AspNetCore_MVC/ViewModels/Components/BoxViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetCore_MVC.ViewModels.Components;
+
+public class BoxViewModel
+{
+    public ImageViewModel BoxImage { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
