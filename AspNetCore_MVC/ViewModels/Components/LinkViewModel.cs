b2be47432cf82debacf9fc07e06190977a0a722d
Anna <nakupanda89@gmail.com>
2024-02-20 14:21:24 +0100

    manage work done

--- AspNetCore_MVC/ViewModels/Components/LinkViewModel.cs
+++ AspNetCore_MVC/ViewModels/Components/LinkViewModel.cs
@@ -5,4 +5,5 @@ public class LinkViewModel
     public string ControllerName { get; set; } = null!;
     public string ActionName { get; set; } = null!;
     public string Text { get; set; } = null!;
+    public string? Icon { get; set; }
 }
