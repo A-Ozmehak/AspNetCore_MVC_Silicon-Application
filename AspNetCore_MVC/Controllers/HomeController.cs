27636e49fb4cea59930117fec6186ea2f57c2083
Anna <nakupanda89@gmail.com>
2024-02-20 09:15:42 +0100

    Add project files.

---
+++ AspNetCore_MVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspNetCore_MVC.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ AspNetCore_MVC/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRouting(x => x.LowercaseUrls = true);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
