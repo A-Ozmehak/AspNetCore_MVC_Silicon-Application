57570ed0dca85a5eecac55b1c029cea05030e17d
Anna <nakupanda89@gmail.com>
2024-02-20 11:10:20 +0100

    showcase done

--- AspNetCore_MVC/Controllers/HomeController.cs
+++ AspNetCore_MVC/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using AspNetCore_MVC.ViewModels.Sections;
+using AspNetCore_MVC.ViewModels.Views;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -5,11 +7,30 @@ namespace AspNetCore_MVC.Controllers;
 
 public class HomeController : Controller
 {
-
-
     public IActionResult Index()
     {
-        return View();
+        var viewModel = new HomeIndexViewModel
+        {
+            Title = "Task Management Assistant",
+            Showcase = new ShowcaseViewModel
+            {
+                Id = "overview",
+                ShowcaseImage = new() { ImageUrl = "/images/showcase-background.svg", AltText = "Task Management Assistant" },
+                Title = "Task Management Assistant You Gonna Love",
+                Text = "We offer you a new generation of task management system. Plan, manage & track all your tasks in one flexible tool.",
+                BrandsText = "Largest companies use our tool to work efficiently.",
+                Link = new() { ControllerName = "Account", ActionName = "SignUp", Text = "Get started for free" },
+                Brands =
+                   [
+                       new() { ImageUrl = "/images/brands/brand1.svg", AltText = "Brand name 1" },
+                       new() { ImageUrl = "/images/brands/brand2.svg", AltText = "Brand name 2" },
+                       new() { ImageUrl = "/images/brands/brand3.svg", AltText = "Brand name 3" },
+                       new() { ImageUrl = "/images/brands/brand4.svg", AltText = "Brand name 4" }
+                   ],
+            }
+        };
+        ViewData["Title"] = viewModel.Title;
+        return View(viewModel);
     }
 
 }
---
+++ AspNetCore_MVC/ViewModels/Views/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore_MVC.ViewModels.Sections;
+
+namespace AspNetCore_MVC.ViewModels.Views;
+
+public class HomeIndexViewModel
+{
+    public string Title { get; set; } = "";
+    public ShowcaseViewModel Showcase { get; set; } = null!;
+}
