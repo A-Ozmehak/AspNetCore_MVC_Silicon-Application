0fc4fe2f52661e2bbd987235296432e7986780a1
Anna <nakupanda89@gmail.com>
2024-02-21 10:07:10 +0100

    sign up done

--- AspNetCore_MVC/Controllers/AuthController.cs
+++ AspNetCore_MVC/Controllers/AuthController.cs
@@ -26,7 +26,23 @@ public class AuthController : Controller
 
         viewModel.ErrorMessage = "Incorrect email or password";
         return View(viewModel);
+    }
 
+    [Route("/signup")]
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
 
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        return RedirectToAction("SignIn", "Auth");
     }
 }
---
+++ AspNetCore_MVC/ViewModels/Views/SignUpViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore_MVC.Models.Views;
+
+namespace AspNetCore_MVC.ViewModels.Views;
+
+public class SignUpViewModel
+{
+    public string Title { get; set; } = "Sign up";
+    public SignUpModel Form { get; set; } = new SignUpModel();
+}
