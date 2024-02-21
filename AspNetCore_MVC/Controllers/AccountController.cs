e0a13e718c6827db7575abe272b5a93a51ac38f3
Anna <nakupanda89@gmail.com>
2024-02-21 10:32:25 +0100

    account details desktop done

---
+++ AspNetCore_MVC/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCore_MVC.ViewModels.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVC.Controllers;
+
+public class AccountController : Controller
+{
+    //private readonly AccountService _accountService;
+
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
+
+    [Route("/account")]
+    public IActionResult Details()
+    {
+        var viewModel = new AccountDetailsViewModel();
+        //viewModel.BasicInfo = _accountService.GetBasicInfo();
+        //viewModel.AddressInfo = _accountService.GetAddressInfo();
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveBasicInfo(viewModel.BasicInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult AddressInfo(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveAddressInfo(viewModel.AddressInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+}
---
+++ AspNetCore_MVC/Models/Components/AccountDetailsBasicInfoModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore_MVC.Models.Components;
+
+public class AccountDetailsBasicInfoModel
+{
+    [DataType(DataType.ImageUrl)]
+    public string? ProfileImage { get; set; }
+
+    [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "First name is required")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Your email addres is invalid")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Phone", Prompt = "Enter your phone", Order = 3)]
+    [DataType(DataType.PhoneNumber)]
+    [Required(ErrorMessage = "Phone is required")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Your phone number is invalid")]
+    public string Phone { get; set; } = null!;
+
+    [Display(Name = "Bio", Prompt = "Add a short bio...", Order = 4)]
+    [DataType(DataType.MultilineText)]
+    public string? Biography { get; set; }
+}
