1eb627c841499073057da3fe4c81c6fc918fc2f6
Anna <nakupanda89@gmail.com>
2024-02-21 09:59:49 +0100

    signin done

---
+++ AspNetCore_MVC/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCore_MVC.ViewModels.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVC.Controllers;
+
+public class AuthController : Controller
+{
+    [Route("/signin")]
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        var viewModel = new SignInViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signin")]
+    [HttpPost]
+    public IActionResult SignIn(SignInViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        //var result = _authService.SignIn(viewModel.Form);
+        //if (result)
+        //    return RedirectToAction("Account", "Index");
+
+        viewModel.ErrorMessage = "Incorrect email or password";
+        return View(viewModel);
+
+
+    }
+}
