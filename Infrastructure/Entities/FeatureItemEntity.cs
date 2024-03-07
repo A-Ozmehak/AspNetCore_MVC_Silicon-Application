54b197d27b03bfb28d70f1e79c96fa391b8f2b73
Anna <nakupanda89@gmail.com>
2024-03-07 14:58:29 +0100

    set up the database and added entities

---
+++ Infrastructure/Entities/FeatureItemEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Entities;
+
+public class FeatureItemEntity
+{
+    public int Id { get; set; }
+
+    public int FeatureId { get; set; }
+    public FeatureEntity Feature { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Text { get; set; } = null!;
+}
