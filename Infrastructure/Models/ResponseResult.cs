80a637aa92c9b4ccfba49ab2df1fd626e675b998
Anna <nakupanda89@gmail.com>
2024-03-07 14:59:05 +0100

    set up different status messages

---
+++ Infrastructure/Models/ResponseResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Models;
+
+public enum StatusCode
+{
+    OK = 200,
+    ERROR = 400,
+    NOT_FOUND = 409,
+    EXISTS = 409
+}
+
+public class ResponseResult
+{
+    public StatusCode Status { get; set; }
+    public object? ContentResult { get; set; }
+    public string? Message { get; set; }
+}
