4dafc819202aeeb236e295e76d0e35deb21eb355
Anna <nakupanda89@gmail.com>
2024-03-07 20:21:55 +0100

    removed content from controller and added to the view

--- AspNetCore_MVC/Controllers/HomeController.cs
+++ AspNetCore_MVC/Controllers/HomeController.cs
@@ -11,26 +11,7 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        //var viewModel = new HomeIndexViewModel
-        //{
-        //    Title = "Task Management Assistant",
-        //    Slider = new SliderViewModel
-        //    {
-        //        Title = "Switch Between",
-        //        Description = "Light & Dark Mode",
-        //        Image = new() { ImageUrl = "/images/mockup.svg", AltText = ""}
-        //    },
-   
-        //    Newsletter = new NewsletterViewModel
-        //    {
-        //        Title = "Don't Want to Miss Anything?",
-        //        Arrow = new() { ImageUrl = "/images/arrows.svg", AltText = "Arrow"},
-        //        SignUpText = "Sign up for Newsletters",
-        //        Link = new() { Text = "Subscribe*", ActionName = "Subscribe", ControllerName = "Home" },
-        //    }
-        //};      
-
-        //ViewData["Title"] = viewModel.Title;
+        ViewData["Title"] = "Task Management Assistant";
         return View();
     }
 
