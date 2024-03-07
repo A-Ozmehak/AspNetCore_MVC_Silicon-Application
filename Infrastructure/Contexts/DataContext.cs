c4e58e7c5197fdb1de2c0c4bbe4f8068aef024cc
Anna <nakupanda89@gmail.com>
2024-03-07 14:59:46 +0100

    added migration, context, and repo

---
+++ Infrastructure/Contexts/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<AddressEntity> Addresses { get; set; }
+    public DbSet<FeatureEntity> Features { get; set; }
+    public DbSet<FeatureItemEntity> FeatureItems { get; set; }
+}
---
+++ Infrastructure/Repositories/Repo.cs
@@ -0,0 +1,109 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public abstract class Repo<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+
+    public virtual async Task<ResponseResult> CreateOneAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return ResponseFactory.Ok(entity);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> GetAllAsync()
+    {
+        try
+        {
+            IEnumerable<TEntity> result = await _context.Set<TEntity>().ToListAsync();
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> GetOneAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result == null)
+                return ResponseFactory.NotFound();
+
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> UpdateOneAsync(Expression<Func<TEntity, bool>> predicate, TEntity updatedEntity)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Entry(result).CurrentValues.SetValues(updatedEntity);
+                await _context.SaveChangesAsync();
+                return ResponseFactory.Ok(result);
+            }
+            return ResponseFactory.NotFound();
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> DeleteOneAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Set<TEntity>().Remove(result);
+                await _context.SaveChangesAsync();
+                return ResponseFactory.Ok("Successfully removed");
+            }
+            return ResponseFactory.NotFound();
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> AlreadyExistsAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().AnyAsync(predicate);
+            if (result)
+                return ResponseFactory.Exists();
+
+            return ResponseFactory.NotFound();
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+}
