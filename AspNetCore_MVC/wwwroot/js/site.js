80809a234f9c997229ac3ebfd98b4bf76196fe42
Anna <nakupanda89@gmail.com>
2024-03-25 08:57:26 +0100

    validation

--- AspNetCore_MVC/wwwroot/js/site.js
+++ AspNetCore_MVC/wwwroot/js/site.js
@@ -104,3 +104,99 @@ document.querySelector('#profileImage').addEventListener('click', function () {
 document.querySelector('#fileInput').addEventListener('change', function () {
     document.querySelector('#uploadForm').submit();
 });
+
+
+
+
+
+
+
+const formErrorHandler = (element, validationResult) => {
+    let spanElement = document.querySelector(`[data-valmsg-for="${element.name}"]`)
+
+    if (validationResult) {
+        element.classList.remove('input-validation-error')
+        spanElement.classList.remove('field-validation-error')
+        spanElement.classList.add('field-validation-valid')
+        spanElement.innerHTML = ''
+    }
+    else {
+        element.classList.add('input-validation-error')
+        spanElement.classList.add('field-validation-error')
+        spanElement.classList.remove('field-validation-valid')
+        spanElement.innerHTML = element.dataset.valRequired
+    }
+
+}
+
+const compareValidator = (element, compareValue) => {
+    if (element.value === compareValue)
+        return true;
+
+    return false;
+}
+
+const textValidator = (element, minLength = 2) => {
+    if (element.value.length >= minLength) {
+        formErrorHandler(element, true)
+    }
+    else {
+        formErrorHandler(element, false)
+    }
+}
+
+const emailValidator = (element) => {
+    const regEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    formErrorHandler(element, regEx.test(element.value))
+}
+
+const passwordValidator = (element) => {
+    if (element.dataset.valEqualtoOther !== undefined) {
+        let password = document.getElementsByName(element.dataset.valEqualtoOther.replace('*', 'Form'))[0].value
+
+        if (element.value === password)
+            formErrorHandler(element, true)
+        else
+            formErrorHandler(element, false)
+    }
+    else {
+        const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,})/;
+        formErrorHandler(element, regEx.test(element.value))
+    }
+}
+
+const checkboxValidator = (element) => {
+    if (element.checked)
+        formErrorHandler(element, true)
+    else
+        formErrorHandler(element, false)
+}
+
+let forms = document.querySelectorAll('form')
+let inputs = forms[0].querySelectorAll('input')
+
+inputs.forEach(input => {
+    if (input.dataset.val === 'true') {
+
+        if (input.type === 'checkbox') {
+            input.addEventListener('change', (e) => {
+                checkboxValidator(e.target)
+            })
+        }
+        else {
+            input.addEventListener('keyup', (e) => {
+                switch (e.target.type) {
+                    case 'text':
+                        textValidator(e.target)
+                        break;
+                    case 'email':
+                        emailValidator(e.target)
+                        break;
+                    case 'password':
+                        passwordValidator(e.target)
+                        break;
+                }
+            })
+        }
+    }
+})
